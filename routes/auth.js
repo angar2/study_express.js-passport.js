6f8ddfc43e2276fa8f6607319a59bd6839b7fa85
앵가리 <>
2023-01-19 18:43:51 +0900

    회원가입 UI

--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,28 @@ const passport = require('passport');
 const template = require('../lib/template.js');
 const auth = require('../lib/auth.js');
 
+router.get('/signup', (request, response) => {
+    var fmsg = request.flash();
+    var feedback = '';
+    if(fmsg.error) {
+        feedback = `<p>${fmsg.error[0]}</p>`;
+    }
+    var title = 'Sign up';
+    var list = template.list(request.filelist);
+    var HTML = template.HTML(title, list,
+        `<form action="/auth/signup" method="post">
+            <p><input type="text" name="email" placeholder="Email" /></p>
+            <p><input type="password" name="password" placeholder="Password" /></p>
+            <p><input type="password" name="password2" placeholder="Confirm Password" /></p>
+            <p><input type="text" name="displayName" placeholder="Display Name" /></p>
+            <p><input type="submit" value="Sign up" /></p>
+        </form>`,
+        `<h2>${title}</h2>`,
+        auth.Status(request, response)
+    );
+    response.send(HTML);
+});
+
 router.get('/login', (request, response) => {
     var fmsg = request.flash();
     var feedback = '';
